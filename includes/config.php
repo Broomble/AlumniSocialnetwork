7ce1166ad18885d21bdad677ec72f63763423b86
Navneet Pandey <npandey057@gmail.com>
2014-04-07 14:32:24 +0530

    first commit

---
+++ includes/config.php
@@ -0,0 +1,34 @@
+<?php
+#error_reporting(0);
+error_reporting(E_ALL ^ E_NOTICE);
+
+$CONF = $TMPL = array();
+
+// The MySQL credentials
+$CONF['host'] = 'localhost';
+$CONF['user'] = 'root';
+$CONF['pass'] = 'root';
+$CONF['name'] = 'alumni';
+
+// The Installation URL
+$CONF['url'] = 'http://alumni.dev';
+
+// The Notifications e-mail
+$CONF['email'] = 'notifications@alumni.dev';
+
+// The themes directory
+$CONF['theme_path'] = 'themes';
+
+$action = array('admin'			=> 'admin',
+				'feed'			=> 'feed',
+				'settings'		=> 'settings',
+				'messages'		=> 'messages',
+				'post'			=> 'post',
+				'recover'		=> 'recover',
+				'timeline'		=> 'timeline',
+				'profile'		=> 'profile',
+				'notifications'	=> 'notifications',
+				'search'		=> 'search',
+				'page'			=> 'page'
+				);
+?>
---
+++ themes/dolphin/info.php
@@ -0,0 +1,13 @@
+<?php
+// Theme Name
+$name = 'Dolphin';
+
+// Theme Author
+$author = 'Pricop Alexandru - Mihai';
+
+// Theme URL
+$url = 'http://www.phpdolphin.com/';
+
+// Theme Version
+$version = '1.2.3';
+?>
